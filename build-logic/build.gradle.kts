480a9e8341d841e1cbc2f7731ba4ab29bba22afe
kartega <kartega@example.com>
2025-10-24 16:59:02 +0300

    son çalışan kütüphanem

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
     mavenCentral()
     gradlePluginPortal()
 }
+
 gradlePlugin {
     plugins {
         register("androidApplication") {
@@ -31,15 +32,9 @@ gradlePlugin {
 }
 
 dependencies {
-    // Android Gradle Plugin
-    implementation("com.android.tools.build:gradle:8.9.1")
-
-    implementation("org.jetbrains.kotlin:kotlin-serialization:2.0.21")
-
-
-    // Kotlin Gradle Plugin
-    implementation(kotlin("gradle-plugin", version = "2.0.21"))
-    implementation("com.google.dagger:hilt-android-gradle-plugin:2.51")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.28")
-
+    implementation(libs.android.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.serialization.plugin)
+    implementation(libs.ksp.gradle.plugin)
+    implementation(libs.hilt.gradle.plugin)
 }

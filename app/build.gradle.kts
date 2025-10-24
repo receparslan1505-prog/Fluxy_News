dd8672e52c77dee4cc462e3ba332ddacf87f78fa
kartega <kartega@example.com>
2025-10-24 17:00:16 +0300

    invalid libs deleted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,4 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
